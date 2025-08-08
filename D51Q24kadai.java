c0c65fa1dba6f613dd8aad208e3da6cf7b8b464e
s24004 <s24004@std.it-college.ac.jp>
2025-08-08 20:57:17 +0900

    提出

---
+++ D51Q24kadai.java
@@ -0,0 +1,7 @@
+public class D51Q24kadai {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+           if (i % 15 == 0) System.out.println(i);
+        }
+    }
+}

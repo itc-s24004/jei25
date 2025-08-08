e5e3d642a5bbe1daec72adb794c3a2368f7a0877
s24004 <s24004@std.it-college.ac.jp>
2025-08-08 21:11:34 +0900

    提出

---
+++ D51Q26kadai.java
@@ -0,0 +1,22 @@
+public class D51Q26kadai {
+    public static void main(String[] args) {
+        System.out.println("one");
+        //caseはswitch文の条件を定義するために使用されます
+        //breakはforなどのループやswitchで使用されます
+        //ループではループの終了
+        //switchでは処理の終了
+        //fall-throughはswitch文での処理を終了せずに次の処理に移す
+        //例▼
+//        int version = 1;
+//        switch (version) {
+//            case 1:
+//                System.out.println("更新 1->2");
+//            case 2:
+//                System.out.println("更新 2->3");
+//            case 3:
+//                System.out.println("更新 3->4");
+//                System.out.println("最新になりました");
+//                break;
+//        }
+    }
+}
